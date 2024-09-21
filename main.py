ad0235d2abe35c36e415a6c57b39d97cdd6e1ffa
BlackWolf-01 <jesehitaen@gmail.com>
2024-09-21 20:09:03 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+def printnumber(n):
+    i = 0
+    print('The number entered by the user is',n)
+    i+=1
+    print('Total iterations created by the code is',i)
+printnumber(15)
+printnumber(82)
+print('with any n the time taken by our code wont change')
